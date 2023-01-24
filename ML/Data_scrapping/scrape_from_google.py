05e51d4017a512f3f39ae6d41f29a896014fd222
avs-abhishek123 <abhishek.avs123@gmail.com>
2023-01-24 15:36:13 +0530

    text summarization shrey added

--- ML/Data_scrapping/scrape_from_google.py
+++ ML/Data_scrapping/scrape_from_google.py
@@ -25,8 +25,8 @@ The function fetch_image_urls expects three input parameters:
 def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_between_interactions:int=1):
     def scroll_to_end(wd):
         wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(sleep_between_interactions)    
-    
+        time.sleep(sleep_between_interactions)
+
     # build the google query
     search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
 
@@ -42,9 +42,9 @@ def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_betw
         # get all image thumbnail results
         thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
         number_results = len(thumbnail_results)
-        
+
         print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
-        
+
         for img in thumbnail_results[results_start:number_results]:
             # try to click every thumbnail such that we can get the real image behind it
             try:
@@ -53,7 +53,7 @@ def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_betw
             except Exception:
                 continue
 
-            # extract image urls    
+            # extract image urls
             actual_images = wd.find_elements_by_css_selector('img.n3VNCb')
             for actual_image in actual_images:
                 if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
